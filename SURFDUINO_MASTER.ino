4b62306ce8f0ef49ee6546d565294693b4ab65c8
Jordan Hill <jordanehill01@gmail.com>
2018-04-14 18:53:46 -0400

    reworked to send slave data and take actions as a result of the ble commands data received
    Signed-off-by: Jordan Hill <jordanehill01@gmail.com>

--- SURFDUINO_MASTER.ino
+++ SURFDUINO_MASTER.ino
@@ -19,6 +19,11 @@ uint8_t DATA_OBJ_SZ_M0 = sizeof(Data);
 //we trigger logging above this speed
 #define LOGGING_SPEED 5
 
+//BLE commands
+static const char BLE_SAVE_DATA_ON  = 'O';
+static const char BLE_SAVE_DATA_OFF = 'F';
+static const char BLE_SYS_STATUS    = 'S';
+static const char BLE_STATS_REPORT  = 'R';
 
 //data from slave
 SlaveDataStruct slvData;
@@ -37,6 +42,10 @@ bool setFileName = true;
 String fileName;
 //how many times we trigger recording
 uint8_t instancesOfRecording = 0;
+//controls whether we save data or not
+bool saveDataOn = false;
+//track when to request slave data as it only changes every second
+unsigned int lastRequestMillis = millis();
 
 //buffers and related index
 Data dataObjBuffer[BUFFER_SZ];
@@ -50,25 +59,107 @@ uint8_t bufferIdx = 0;
 //writes buffers to SD card
 void writeBuffersToSD()
 {
-    uint8_t saveBufferIdx = 0;
-
-    file = SD.open(fileName, FILE_WRITE);
-    while(saveBufferIdx < bufferIdx)
+    if(saveDataOn)
     {
-        file.print(instancesOfRecording);file.print(",");
-        file.print(slaveDataBuffer[saveBufferIdx].timeString);file.print(",");
-        file.print(slaveDataBuffer[saveBufferIdx].mph,4);file.print(",");
-        file.print(dataObjBuffer[saveBufferIdx].getHeading());file.print(",");
-        file.print(dataObjBuffer[saveBufferIdx].getRateOfTurn(),4);file.print(",");
-        file.print(dataObjBuffer[saveBufferIdx].getPitchAngle(),4);file.print(",");
-        file.print(dataObjBuffer[saveBufferIdx].getRollAngle(),4);file.print(",");
-        file.print(slaveDataBuffer[saveBufferIdx].latitude,4);file.print(",");
-        file.print(slaveDataBuffer[saveBufferIdx].longitude,4);file.println();
-        saveBufferIdx++;
+        uint8_t saveBufferIdx = 0;
+
+        file = SD.open(fileName, FILE_WRITE);
+        while(saveBufferIdx < bufferIdx)
+        {
+            file.print(instancesOfRecording);file.print(",");
+            file.print(slaveDataBuffer[saveBufferIdx].timeString);file.print(",");
+            file.print(slaveDataBuffer[saveBufferIdx].mph,4);file.print(",");
+            file.print(dataObjBuffer[saveBufferIdx].getHeading());file.print(",");
+            file.print(dataObjBuffer[saveBufferIdx].getRateOfTurn(),4);file.print(",");
+            file.print(dataObjBuffer[saveBufferIdx].getPitchAngle(),4);file.print(",");
+            file.print(dataObjBuffer[saveBufferIdx].getRollAngle(),4);file.print(",");
+            file.print(slaveDataBuffer[saveBufferIdx].latitude,4);file.print(",");
+            file.print(slaveDataBuffer[saveBufferIdx].longitude,4);file.println();
+            saveBufferIdx++;
+        }
+        file.close();     
     }
-    file.close();
-    
-    bufferIdx = 0;
+    bufferIdx = 0;   
+}
+
+//create SD file with a header record
+void createSDFile()
+{
+    //once gps locked - set file name to current date
+    if (setFileName && slvData.isGpsLocked && saveDataOn)
+    {   
+        //filename is utc date from gps
+        fileName = slvData.dateString;
+        fileName = fileName + ".log";
+        setFileName = false;
+
+        //print header
+        file = SD.open(fileName, FILE_WRITE);
+        file.println("record_id,utc_time,mph,bearing,turn_rate,pitch_angle,roll_angle,latitude,longitude");
+        file.close();
+    }
+}
+
+//BLE command actions
+void processBleCommand()
+{
+  if(slvData.bleCmd == BLE_SAVE_DATA_ON)
+  {
+      saveDataOn = true;
+      Wire.beginTransmission(I2C_SLAVE_ADDRESS);
+      Wire.write("OK");
+      uint8_t e =Wire.endTransmission();
+  }
+  else if(slvData.bleCmd == BLE_SAVE_DATA_OFF)
+  {
+      saveDataOn = false;
+      Wire.beginTransmission(I2C_SLAVE_ADDRESS);
+      Wire.write("OK");
+      uint8_t e = Wire.endTransmission();
+  }
+  else if(slvData.bleCmd == BLE_SYS_STATUS)
+  {
+      //prepare to send
+      Wire.beginTransmission(I2C_SLAVE_ADDRESS);
+  
+      //--------------------
+      //   record mode
+      //--------------------
+      Wire.write("RECORDING:");
+      if(saveDataOn)
+          Wire.write("ON");
+      else
+          Wire.write("OFF");
+      Wire.write(",BATTERY:");
+      //--------------------
+      //   power
+      //--------------------
+      float measuredvbat = analogRead(A7);
+      measuredvbat *= 2;    // we divided by 2, so multiply back
+      measuredvbat *= 3.3;  // Multiply by 3.3V, our reference voltage
+      measuredvbat /= 1024; // convert to voltage
+      if(measuredvbat< 3.7)
+          Wire.write("LOW");
+      else
+          Wire.write("OK");
+      //--------------------
+      //   gps lock
+      //--------------------
+      Wire.write(",GPS:");
+      if(slvData.isGpsLocked)
+          Wire.write("LOCK");
+      else
+          Wire.write("SEEK");
+  
+      //send data
+      Wire.endTransmission();     
+  }
+  else if(slvData.bleCmd == BLE_STATS_REPORT)
+  {
+  
+                            
+  }
+  slvData.bleCmd = '*';
 }
 
 //runs once
@@ -104,70 +195,58 @@ void loop() {
                         }
                         //*****************************
 
-    //get gps data over I2C
-    uint8_t bytes =  Wire.requestFrom(I2C_SLAVE_ADDRESS, SIZE_SLV_DATA);
-    uint8_t i2cBuffer[SIZE_SLV_DATA_M0];
-    uint8_t i2cBufferIdx = 0;
-
-    //get GPS and BLE data from slave node
-    while (Wire.available())                            
+    //get gps/ble data over I2C every second
+    if(millis() - lastRequestMillis >= 1000)
     {
+      //set last request time
+      lastRequestMillis = millis();
+      
+      //request data
+      uint8_t bytes =  Wire.requestFrom(I2C_SLAVE_ADDRESS, SIZE_SLV_DATA);
+      uint8_t i2cBuffer[SIZE_SLV_DATA_M0];
+      uint8_t i2cBufferIdx = 0;
+
+      //get GPS and BLE data from slave node
+      while (Wire.available())                            
+      {
         i2cBuffer[i2cBufferIdx] = Wire.read();       
         i2cBufferIdx++;                                
+      }
+
+      //the slave is 8bit and we have to memcpy
+      //each variable as the SAMD creates a 32 byte
+      //memory space for the data structure
+      //if you just memcpy the whole 28 bytes (slave fits the 
+      //data on single word bounaries) at once we get errors
+      //memcpy takes account for the padding the SAMD does 
+      //to keep everything on 2 byte address boundaries
+      //so this gets the data in our struct correctly
+      memcpy(&slvData.dateString,i2cBuffer,7);
+      memcpy(&slvData.timeString,i2cBuffer+7,7);
+      memcpy(&slvData.mph,i2cBuffer+14,4);
+      memcpy(&slvData.latitude,i2cBuffer+18,4);
+      memcpy(&slvData.longitude,i2cBuffer+22,4);
+      memcpy(&slvData.bleCmd,i2cBuffer+26,1);
+      memcpy(&slvData.isGpsLocked,i2cBuffer+27,1);
     }
-    
-    //the slave is 8bit and we have to memcpy
-    //each variable as the SAMD creates a 32 byte
-    //memory space for the data structure
-    //if you just memcpy the whole 28 bytes (slave fits the 
-    //data on single word bounaries) at once we get errors
-    //memcpy takes account for the padding the SAMD does 
-    //to keep everything on 2 byte address boundaries
-    //so this gets the data in our struct correctly
-    memcpy(&slvData.dateString,i2cBuffer,7);
-    memcpy(&slvData.timeString,i2cBuffer+7,7);
-    memcpy(&slvData.mph,i2cBuffer+14,4);
-    memcpy(&slvData.latitude,i2cBuffer+18,4);
-    memcpy(&slvData.longitude,i2cBuffer+22,4);
-    memcpy(&slvData.bleCmd,i2cBuffer+26,1);
-    memcpy(&slvData.isGpsLocked,i2cBuffer+27,1);
-   
+
     //read/update from the imu
     imuObj.updateValues(dataObj);   
 
-    //once gps locked - set file name to current date
-    if (setFileName && slvData.isGpsLocked)
-    {   
-        //filename is utc date from gps
-        fileName = slvData.dateString;
-        fileName = fileName + ".log";
-        setFileName = false;
+    //create SD file (runs once - when gps is locked and when recording is turned on)     
+    createSDFile();
 
-        //print header
-        file = SD.open(fileName, FILE_WRITE);
-        file.println("record_id,utc_time,mph,bearing,turn_rate,pitch_angle,roll_angle,latitude,longitude");
-        file.close();
-    }
-
-                if (slvData.isGpsLocked)
-                {   
-                    Serial.print("L");
-                }
-                else
-                {
-                    Serial.print("U");
-                }
     //if the gps indicates we are riding a wave (based on speed) then log data
     //if ( slvData.gpsLocked && (slvData.mph >= LOGGING_SPEED )) 
                              //***********************************
                             if (slvData.isGpsLocked && writesd)
                             //***********************************
     {   
-        //open the file once we start recording data   
+        //if we are recording a new ride - increment record count
+        //and set record mode to true  
         if(recordMode == false)
         {
             instancesOfRecording++;
-            //--file = SD.open(fileName, FILE_WRITE);
             recordMode = true;
         }
                             //***********************************
@@ -178,6 +257,12 @@ void loop() {
         memcpy(&dataObjBuffer[bufferIdx],&dataObj,DATA_OBJ_SZ_M0);
         memcpy(&slaveDataBuffer[bufferIdx],&slvData,SIZE_SLV_DATA_M0);
         bufferIdx++;
+
+        //write buffers to SD if we have filled them
+        if (bufferIdx == BUFFER_SZ)
+        {
+            writeBuffersToSD();
+        }
  
     }
    else if (recordMode)
@@ -190,13 +275,13 @@ void loop() {
         //write buffers to SD
         writeBuffersToSD();      
     }
-
-    //write buffers to SD if we have filled them
-    if (bufferIdx == BUFFER_SZ)
-    {
-        writeBuffersToSD();
+    else
+    { 
+        //if nothing is happening process a ble command
+        //process ble commands
+        processBleCommand(); 
     }
-
+    
     //0.2 second delay
     delay(200);
 }
